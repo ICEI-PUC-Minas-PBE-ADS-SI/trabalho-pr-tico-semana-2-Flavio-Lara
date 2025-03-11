47a4cd8f71381f0884043ba0ef409636a4489c37
Flavio-Lara <153305877+Flavio-Lara@users.noreply.github.com>
2025-03-10 21:42:47 -0300

    Menu Lateral

---
+++ script.js
@@ -0,0 +1,7 @@
+function abrirMenu() {
+    document.getElementById("menuLateral").style.width = "250px";
+}
+
+function fecharMenu() {
+    document.getElementById("menuLateral").style.width = "0";
+}
